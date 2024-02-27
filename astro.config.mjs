6b1cf1740416febe2500e689c338c19df69a10bf
Mindevs.Tech <snock.mind@gmail.com>
2024-02-27 13:14:37 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://mindevs.github.io",
+    base: '/mindevs.github.io',
   integrations: [tailwind(), icon()],
 });
